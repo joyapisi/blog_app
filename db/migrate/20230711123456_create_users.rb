d850366c2e043706784039f0330a50debaee0afb
Joy Phoebe <joyapisi@gmail.com>
2023-07-11 23:09:20 +0300

    populate create_users migration file

---
+++ db/migrate/20230711123456_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+    def change
+      create_table :users do |t|
+        t.string :name
+        t.string :photo
+        t.text :bio
+        t.integer :posts_counter, default: 0
+        t.timestamps
+      end
+    end
+  end
+  
