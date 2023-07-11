7cccefbdcd80341ca6ecae28df89a3fb58509315
Joy Phoebe <joyapisi@gmail.com>
2023-07-12 00:49:52 +0300

    Fix linter errors

--- db/migrate/20230711123456_create_comments.rb
+++ db/migrate/20230711123456_create_comments.rb
@@ -1,14 +1,13 @@
 class CreateComments < ActiveRecord::Migration[6.1]
-    def change
-      create_table :comments do |t|
-        t.references :author, null: false, foreign_key: { to_table: :users }
-        t.references :post, null: false, foreign_key: true
-        t.string :title
-        t.text :text
-        t.timestamps
-      end
-  
-      add_index :comments, [:author_id, :post_id]
+  def change
+    create_table :comment do |t|
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.references :post, null: false, foreign_key: true
+      t.string :title
+      t.text :text
+      t.timestamps
     end
+
+    add_index :comment, %i[author_id post_id]
   end
-  
+end
--- db/migrate/20230711123456_create_likes.rb
+++ db/migrate/20230711123456_create_likes.rb
@@ -1,11 +1,11 @@
 class CreateLikes < ActiveRecord::Migration[6.1]
-    def change
-      create_table :likes do |t|
-        t.references :author, null: false, foreign_key: { to_table: :users }
-        t.references :post, null: false, foreign_key: true
-        t.timestamps
-      end
-  
-      add_index :likes, [:author_id, :post_id], unique: true
+  def change
+    create_table :like do |t|
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.references :post, null: false, foreign_key: true
+      t.timestamps
     end
+
+    add_index :like, %i[author_id post_id], unique: true
   end
+end
