f74e43facf59165defa04d37f7872e14491810ce
Joy Phoebe <joyapisi@gmail.com>
2023-07-13 20:04:14 +0300

    add post model and 2 methods in it

---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+
+  def update_user_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def recent_comments
+    comments.order("created_at DESC").limit(5)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  def recent_posts
+    posts.order("created_at DESC").limit(3)
+  end
 end
