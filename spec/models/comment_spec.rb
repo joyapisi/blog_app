a96b51180f011be237b725f1908eaa8f10e9e69a
Joy Phoebe <joyapisi@gmail.com>
2023-07-14 22:03:03 +0300

    Fix linter errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-    describe 'associations' do
-      it { should belong_to(:author).class_name('User') }
-      it { should belong_to(:post) }
-    end
-  
-    describe '#update_comment_posts_counter' do
-      let(:post) { create(:post) }
-      let(:comment) { create(:comment, post: post) }
-  
-      it 'updates the post comments counter' do
-        expect { comment.update_comment_posts_counter }.to change { post.reload.comments_counter }.by(1)
-      end
+  describe 'associations' do
+    it { should belong_to(:author).class_name('User') }
+    it { should belong_to(:post) }
+  end
+
+  describe '#update_comment_posts_counter' do
+    let(:post) { create(:post) }
+    let(:comment) { create(:comment, post: post) }
+
+    it 'updates the post comments counter' do
+      expect { comment.update_comment_posts_counter }.to change { post.reload.comments_counter }.by(1)
     end
   end
-  
+end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-    describe 'associations' do
-      it { should belong_to(:author).class_name('User') }
-      it { should belong_to(:post).class_name('Post') }
-    end
-  
-    describe '#update_like_posts_counter' do
-      let(:post) { create(:post) }
-      let(:like) { create(:like, post: post) }
-  
-      it 'updates the post likes counter' do
-        expect { like.update_like_posts_counter }.to change { post.reload.likes_counter }.by(1)
-      end
+  describe 'associations' do
+    it { should belong_to(:author).class_name('User') }
+    it { should belong_to(:post).class_name('Post') }
+  end
+
+  describe '#update_like_posts_counter' do
+    let(:post) { create(:post) }
+    let(:like) { create(:like, post: post) }
+
+    it 'updates the post likes counter' do
+      expect { like.update_like_posts_counter }.to change { post.reload.likes_counter }.by(1)
     end
   end
-  
+end
