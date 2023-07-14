ccf861324fc1dcec0ca47c806f888f053d66442a
Joy Phoebe <joyapisi@gmail.com>
2023-07-14 13:56:12 +0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
-  has many :comments, class_name: 'Comment' foreign_key: 'author_id'
+  has many :comments, class_name: 'Comment', foreign_key: 'author_id'
 
   def recent_posts
     posts.order('created_at DESC').limit(3)
