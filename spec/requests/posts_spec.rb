97e690d8da680c982a8159d2be460b9ad18bf8ce
Joy Phoebe <joyapisi@gmail.com>
2023-07-19 08:58:47 +0300

    Add request spec in spec folder

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require '../rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
@@ -6,19 +6,32 @@ RSpec.describe "Posts", type: :request do
       get posts_index_path
       expect(response).to have_http_status(200)
     end
-  end
-end
-
-# spec/requests/posts_request_spec.rb
-
-RSpec.describe "Posts", type: :request do
-  describe "GET #index" do
-    it "returns a successful response" do
-      get posts_path
-      expect(response).to have_http_status(:success)
-    end
 
     it "renders the index template" do
       get posts_path
       expect(response).to render_template(:index)
     end
+   
+    it "includes correct placeholder text in the response body" do
+      get posts_path
+      expect(response.body).to include("Posts here")
+    end
+  end
+
+  describe "GET #show" do
+    it "returns a successful response" do
+      get post_path(2)
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the show template" do
+      get post_path(2)
+      expect(response).to render_template(:show)
+    end
+
+    it "includes correct placeholder text in the response body" do
+      get post_path(2)
+      expect(response.body).to include("Posts Show")
+    end
+  end
+end
