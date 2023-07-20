2a44ec63302b1833340f30b64a365538e2a26c9d
Joy Phoebe <joyapisi@gmail.com>
2023-07-20 22:12:34 +0300

    Add erb html for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
 
+  scope :recent_posts, ->(user) { user.posts.order('created_at DESC').limit(3) }
+
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def recent_posts
-    posts.order('created_at DESC').limit(3)
-  end
+  # def recent_posts
+  #   posts.order('created_at DESC').limit(3)
+  # end
 end
