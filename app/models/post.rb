1a425eff50fed0d9d692a35c27be563ab807f8f5
Joy Phoebe <joyapisi@gmail.com>
2023-07-20 22:15:11 +0300

    Adjust recent_comments and recent_posts methods in posts & users models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  scope :recent_comments, ->(post) { post.comments.order('created_at DESC').limit(5) }
+
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -11,7 +13,4 @@ class Post < ApplicationRecord
     author.update(posts_counter: author.posts.count)
   end
 
-  def recent_comments
-    comments.order('created_at DESC').limit(5)
-  end
 end
