938cefb5b0e7ee295957f2a9524edf872a6817b4
Joy Phoebe <joyapisi@gmail.com>
2023-07-21 04:05:37 +0300

    Remove commented code

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,21 +15,7 @@ class Post < ApplicationRecord
   private
 
   def increment_author_posts_counter
-    # Use self.author to directly access the associated author without an additional database query.
     author.increment(:update_user_posts_counter).save
   end
 end
 
-# class Post < ApplicationRecord
-#   belongs_to :author, class_name: 'User'
-#   has_many :comments, foreign_key: :post_id
-#   has_many :likes, foreign_key: :post_id
-
-#   before_save -> { User.find_by(id: author.id).increment!(:update_user_posts_counter) }
-
-#   scope :recent_comments, ->(post) { post.comments.order('created_at DESC').limit(5) }
-
-#   validates :title, presence: true, length: { maximum: 250 }
-#   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-#   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-# end
