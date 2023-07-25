cfb7bebac4b5d9b0d6c16a3b3223f3805375292b
Joy Phoebe <joyapisi@gmail.com>
2023-07-25 06:02:36 +0300

    Fix capybyra spec for posts#new view

--- spec/views/posts/new_spec.rb
+++ spec/views/posts/new_spec.rb
@@ -1,7 +1,6 @@
-RSpec.describe 'comments/new', type: :system do
-    it 'renders the create new comment form' do
-      visit new_comment_path
-      expect(page).to have_content('New Comment')
+RSpec.describe 'posts/new', type: :system do
+    it 'renders the new post form' do
+        visit post_new_path
+        expect(page).to have_content('New Post')
     end
   end
-  
