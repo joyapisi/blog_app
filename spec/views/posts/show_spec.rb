cc548dd68641ae60929b1a1b94498e74473f43dc
Lawrence Amoafo <rekenburg777@gmail.com>
2023-07-25 09:23:00 +0000

    Add test cases to check post page content

---
+++ spec/views/posts/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'posts/show', type: :system do
+  it 'displays a post and its comments correctly' do
+    user = User.create(name: 'John Doe', email: 'john@example.com', password: 'password')
+
+    post = Post.create(author: user, title: 'Post Title', text: 'Post body')
+
+
+    Comment.create(author: user, post:, text: 'Comment message 1')
+    Comment.create(author: user, post:, text: 'Comment message 2')
+
+    visit post_path(post)
+
+    expect(page).to have_content('Post Title')
+    expect(page).to have_content('Post body')
+
+    expect(page).to have_content('Comment message 1')
+    expect(page).to have_content('Comment message 2')
+  end
+end
