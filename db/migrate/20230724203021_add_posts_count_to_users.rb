2a29aa3ee955f35250a9e8cf84ad4712a3412011
Joy Phoebe <joyapisi@gmail.com>
2023-07-27 00:04:33 +0300

    Reduce url length in features spec folder

--- db/migrate/20230724203021_add_posts_count_to_users.rb
+++ db/migrate/20230724203021_add_posts_count_to_users.rb
@@ -1,7 +1,6 @@
 class AddCounterCaches < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :posts_count, :integer, default: 0
-
+  
     # Populate the posts_count column with the current number of associated posts for each user
     User.reset_column_information
     User.find_each { |user| User.update_counters(user.id, posts_count: user.posts.length) }
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,23 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'User', type: :feature do
+RSpec.describe 'Users', type: :feature do
   before(:each) do
-    @wajo = User.create(name: 'Wajo', photo: 'https://images.unsplash.com/photo-1507152832244-10d45c7eda57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80', bio: 'Teacher from Poland',
-                        posts_counter: 0)
-    @tom = User.create(name: 'Tom',
-                       photo: 'https://images.unsplash.com/photo-1504199367641-aba8151af406?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80', bio: 'Developer from Kenya', posts_counter: 0)
+    url1 = 'https://images.unsplash.com/photo-1507152832244-10d45c7eda57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80'
+    url2 = 'https://images.unsplash.com/photo-1504199367641-aba8151af406?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'
+
+    User.create(name: 'Wajo', photo: url1, bio: 'Teacher from Poland', posts_counter: 0)
+    User.create(name: 'Tom', photo: url2, bio: 'Developer from Kenya', posts_counter: 0)
   end
 
-  describe 'user#index' do
+  describe 'users index' do
     before(:each) do
       visit users_path
     end
     it 'should show the username of all other users.' do
-      expect(page).to have_content(@wajo.name)
-      expect(page).to have_content(@tom.name)
+      expect(page).to have_content(user1.name)
+      expect(page).to have_content(user2.name)
     end
     it 'should show the profile picture for each user.' do
-      expect(page).to have_xpath("//img[contains(@src,'https://images.unsplash.com/photo-1507152832244-10d45c7eda57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80')]")
+      expect(page).to have_xpath("//img[contains(@src,'#{url1}')]")
     end
     it 'should show number of posts each user has written.' do
       expect(page).to have_content 'Number of posts: 0'
@@ -25,35 +26,38 @@ RSpec.describe 'User', type: :feature do
 
     it 'should redirected to that user show page, When I click on a user' do
       click_link 'wajo Tsegaye'
-      expect(page).to have_current_path("/users/#{@wajo.id}")
+      expect(page).to have_current_path("/users/#{user1.id}")
     end
   end
 
   describe 'users/show' do
     before(:each) do
-      @wajo = User.create(name: 'Wajo', photo: 'https://images.unsplash.com/photo-1507152832244-10d45c7eda57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80', bio: 'Teacher from Poland',
+      url1 = 'https://images.unsplash.com/photo-1507152832244-10d45c7eda57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80'
+
+      user1 = User.create(name: 'Wajo',
+                          photo: url1, bio: 'Teacher from Poland',
                           posts_counter: 0)
 
-      @post1 = Post.create(title: 'Second post',
-                           text: 'This is my second post',
-                           comments_counter: 0,
-                           likes_counter: 0,
-                           author: @wajo)
+      Post.create(title: 'Second post',
+                  text: 'This is my second post',
+                  comments_counter: 0,
+                  likes_counter: 0,
+                  author: user1)
 
-      @post2 = Post.create(title: 'My last post',
-                           text: 'last',
-                           comment_counter: 0,
-                           likes_counter: 0,
-                           author: @wajo)
+      Post.create(title: 'My last post',
+                  text: 'last',
+                  comment_counter: 0,
+                  likes_counter: 0,
+                  author: user1)
 
-      visit user_path(@wajo.id)
+      visit user_path(user1.id)
     end
     it 'should show the profile picture for each user.' do
-      expect(page).to have_xpath("//img[contains(@src,'https://wajolinktophoto.jpg')]")
+      expect(page).to have_xpath("//img[contains(@src,'#{url1}')]")
     end
     it 'should show the username of all other users.' do
-      expect(page).to have_content(@wajo.name)
-      expect(page).to_not have_content(@kelly.name)
+      expect(page).to have_content(user1.name)
+      expect(page).to_not have_content(kelly.name)
     end
     it 'should show number of posts each user has written.' do
       expect(page).to have_content 'Number of posts: 1'
@@ -68,7 +72,7 @@ RSpec.describe 'User', type: :feature do
       expect(page).to have_content 'See all Posts'
     end
     it 'When I click a users post, it redirects me to that posts show page.' do
-      visit "/users/#{@wajo.id}/posts/#{@post1.id}"
+      visit "/users/#{user1.id}/posts/#{post1.id}"
       expect(page).to have_content 'This is my second post'
     end
     it 'When I click to see all posts, it redirects me to the users posts index page.' do
