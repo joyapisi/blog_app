1b2683c8fd63b157a2e345d660ffd74cbbb0be8e
Lawrence Amoafo <rekenburg777@gmail.com>
2023-07-28 23:10:11 +0000

    Update method to count likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :post, class_name: 'Post', counter_cache: true
+  belongs_to :post
 
   before_save :update_like_posts_counter
 
   def update_like_posts_counter
-    post.increment!(:likes_counter)
+    Post.find_by(id: post_id).increment!(:likes_counter)
   end
 end
