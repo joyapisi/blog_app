346c9a58d775acd403bbf3dbb45a7ee0121f0f7b
Lawrence Amoafo <rekenburg777@gmail.com>
2023-07-28 23:13:50 +0000

    Remove commented texts and unused spaces

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
@@ -8,36 +6,15 @@ class Ability
     user ||= User.new
 
     # Define a few sample abilities
-    can    :read , Post, is_publised: true
-    
+    can :read, Post, is_publised: true
+
     return unless user.present?
+
     can :manage, Post, author_id: user.id
-    can :manage , Comment, author_id: user.id
-    
+    can :manage, Comment, author_id: user.id
+
     return unless user.admin?
+
     can :manage, :all
-  end
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
   end
 end
